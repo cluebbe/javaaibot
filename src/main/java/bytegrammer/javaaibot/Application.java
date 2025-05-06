53c54fd98b85b0e3dd251557cda2f15d0d28ab1e
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-05-06 11:49:29 +0200

    included restlet

--- src/main/java/bytegrammer/javaaibot/Application.java
+++ src/main/java/bytegrammer/javaaibot/Application.java
@@ -1,8 +1,19 @@
 package bytegrammer.javaaibot;
 
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello Restlet!");
+		final Component component = new Component();
+		component.getServers().add(Protocol.HTTP, 8081);
+
+		try {
+			component.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
