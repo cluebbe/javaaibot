a2a5b0d738103428801290728e4b972604d2895b
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-05-07 16:54:46 +0200

    added client response

---
+++ src/main/java/bytegrammer/javaaibot/data/ClientResponse.java
@@ -0,0 +1,19 @@
+package bytegrammer.javaaibot.data;
+
+public class ClientResponse {
+	String answer;
+	public ClientResponse() {
+		
+	}
+	
+	public ClientResponse(String answer) {
+		this.answer = answer;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+}
--- src/main/java/bytegrammer/javaaibot/handler/ChatbotHandler.java
+++ src/main/java/bytegrammer/javaaibot/handler/ChatbotHandler.java
@@ -12,30 +12,45 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import bytegrammer.javaaibot.Constants;
 import bytegrammer.javaaibot.data.ApiRequest;
 import bytegrammer.javaaibot.data.ApiResponse;
+import bytegrammer.javaaibot.data.ClientResponse;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.HttpHeaders;
 
-public class ChatbotHandler extends Restlet{
+public class ChatbotHandler extends Restlet {
 	public void handle(Request request, Response response) {
 
 		Method m = request.getMethod();
 		if (m.equals(Method.POST)) {
-			
+
 			ObjectMapper objectMapper = new ObjectMapper();
 			ApiRequest apiRequest = new ApiRequest("Who are you");
 			try {
-				Client client = ClientBuilder.newClient(); 
-			    WebTarget webTarget = client.target("https://api.x.ai").path("/v1/chat/completions"); // REST_URI is a constant containing the URL
-					    
-			    ApiResponse apiResponse = webTarget.request(jakarta.ws.rs.core.MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + Constants.API_KEY)
-			    	.accept(jakarta.ws.rs.core.MediaType.APPLICATION_JSON)
-			    	.post(Entity.entity(apiRequest, jakarta.ws.rs.core.MediaType.APPLICATION_JSON), ApiResponse.class);
-			    
-				response.setEntity(objectMapper.writeValueAsString( apiRequest), MediaType.APPLICATION_ALL_JSON);
-				
+				Client client = ClientBuilder.newClient();
+				WebTarget webTarget = client.target("https://api.x.ai").path("/v1/chat/completions"); // REST_URI is a
+																										// constant
+																										// containing
+																										// the URL
+
+				ApiResponse apiResponse = webTarget.request(jakarta.ws.rs.core.MediaType.APPLICATION_JSON)
+						.header(HttpHeaders.AUTHORIZATION, "Bearer " + Constants.API_KEY)
+						.accept(jakarta.ws.rs.core.MediaType.APPLICATION_JSON)
+						.post(Entity.entity(apiRequest, jakarta.ws.rs.core.MediaType.APPLICATION_JSON),
+								ApiResponse.class);
+
+				if (apiResponse != null && !apiResponse.getChoices().isEmpty()
+						&& (apiResponse.getChoices().get(0) != null)
+						&& (apiResponse.getChoices().get(0).getMessage() != null)
+						&& (apiResponse.getChoices().get(0).getMessage().getContent() != null)
+						&& !apiResponse.getChoices().get(0).getMessage().getContent().isBlank()) {
+					response.setEntity(objectMapper.writeValueAsString(new ClientResponse(apiResponse.getChoices().get(0).getMessage().getContent())), MediaType.APPLICATION_ALL_JSON);
+					
+				} else {
+					throw new IllegalStateException("AI-API responded with empty message");
+				}
+
 			} catch (Exception e) {
 				response.setStatus(Status.SERVER_ERROR_INTERNAL, e);
 				e.printStackTrace();
