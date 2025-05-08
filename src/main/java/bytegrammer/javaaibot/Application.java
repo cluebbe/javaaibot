32d05e933aa0a332c8611be5592c081348f22906
Carlos Luebbe <carlosluebbe@Carloss-MacBook-Air.local>
2025-05-08 21:47:50 +0200

    added authentication

--- src/main/java/bytegrammer/javaaibot/Application.java
+++ src/main/java/bytegrammer/javaaibot/Application.java
@@ -1,8 +1,12 @@
 package bytegrammer.javaaibot;
 
 import org.restlet.Component;
+import org.restlet.data.ChallengeScheme;
 import org.restlet.data.Protocol;
 import org.restlet.routing.Router;
+import org.restlet.security.ChallengeAuthenticator;
+import org.restlet.security.MapVerifier;
+
 import bytegrammer.javaaibot.handler.MainPageHandler;
 import bytegrammer.javaaibot.handler.ChatbotHandler;
 
@@ -14,11 +18,30 @@ public class Application {
 		component.getServers().add(Protocol.HTTP, 8081);
 		
 		final Router router = new Router(component.getContext().
-				createChildContext());
-				router.attach("/", new MainPageHandler());
-				router.attach("/chatbot", new ChatbotHandler());
+		
+	    createChildContext());
 
-				component.getDefaultHost().attach(router);			
+		router.attach("/", new MainPageHandler());
+		
+		ChatbotHandler chatbotHandler = new ChatbotHandler();
+		
+		// Guard the restlet with BASIC authentication.
+		ChallengeAuthenticator guard = new ChallengeAuthenticator(null, ChallengeScheme.HTTP_BASIC, "testRealm");
+		// Instantiates a Verifier of identifier/secret couples based on a simple Map.
+		MapVerifier mapVerifier = new MapVerifier();
+		// Load a single static login/secret pair.
+		mapVerifier.getLocalSecrets().put("carlos", "secret".toCharArray());
+		guard.setVerifier(mapVerifier);
+
+		guard.setNext(chatbotHandler);
+		
+		
+		router.attach("/chatbot", guard);
+		
+	
+		
+
+		component.getDefaultHost().attach(router);			
 
 
 		try {
